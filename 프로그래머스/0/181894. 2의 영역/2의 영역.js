506184c8429401aa7ed586b52a19c33f537450dc
jisu <jaeny96@naver.com>
2024-09-09 00:49:52 +0900

    [level 0] Title: 2의 영역, Time: 6199.64 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    const answer = arr.map((item)=>item);
+    
+    const firstIdx = answer.findIndex((item) => item === 2);
+    let lastIdx = 0;
+    
+    while(answer.findIndex((item) => item === 2) !== -1){
+     const idx = answer.findIndex((item) => item === 2);
+     answer[idx] = 0;
+        
+     if(answer.findIndex((item) => item === 2) === -1) lastIdx = idx; 
+    }    
+    
+    console.log(firstIdx, lastIdx)
+    
+    return firstIdx === -1 ? [-1] : arr.slice(firstIdx,lastIdx+1);
+}
