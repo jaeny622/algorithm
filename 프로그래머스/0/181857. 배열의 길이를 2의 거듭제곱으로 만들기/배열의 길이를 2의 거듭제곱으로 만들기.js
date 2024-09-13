531145c58d870d71c0577e1c82f8318d678cddc8
jisu <jaeny96@naver.com>
2024-09-13 15:20:21 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
@@ -7,14 +7,12 @@ function solution(arr) {
     
     while(flag){
         if(arr.length === 2**idx || arr.length < 2**idx) {
-            console.log(2**idx,arr.length);
             flag = false;
             restLength = 2**idx-arr.length;
         } else idx++;
     }
     
     answer.push(...Array(restLength).fill(0));
-    console.log(answer.length)
     
     return answer;
 }
